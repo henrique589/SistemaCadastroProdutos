b0d3f4b7cb7cf0f398d571e0df4f9c0127d07033
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2023-08-15 11:13:41 -0300

    Criação da classe ProdutoFisico

---
+++ src/model/ProdutoFisico.java
@@ -0,0 +1,36 @@
+package model;
+
+public class ProdutoFisico extends Produto{
+	private Double peso;
+	private Double dimensoes;
+	
+	public ProdutoFisico() {
+	}
+
+	public ProdutoFisico(Integer id, String nome, Double preco, Double peso, Double dimensoes) {
+		super(id, nome, preco);
+		this.peso = peso;
+		this.dimensoes = dimensoes;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getDimensoes() {
+		return dimensoes;
+	}
+
+	public void setDimensoes(Double dimensoes) {
+		this.dimensoes = dimensoes;
+	}
+
+	@Override
+	public String toString() {
+		return "ProdutoFisico [id = " + getId() + ", nome = " + getNome() + ", preço = R$ " + getPreco() + ", peso=" + peso + "Kg, dimensoes=" + dimensoes + "]";
+	}
+}
